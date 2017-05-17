f162d292fa4d3d2fba2876aae38f66bcb819dae0
Dave Marchant <dwfmarchant@gmail.com>
2017-05-17 15:44:22 -0700

    Refactored Canvas, DecayWindow.

--- ATEMview/Canvas.py
+++ ATEMview/Canvas.py
@@ -1,26 +1,25 @@
 
-from PyQt5.QtWidgets import QSizePolicy
+from PyQt5 import QtWidgets, QtCore
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
 
 class Canvas(FigureCanvas):
     """docstring for Canvas"""
+
+    canvasClicked = QtCore.pyqtSignal(dict, name='canvasClicked')
+
     def __init__(self, parent=None, width=5, height=4, dpi=100):
-        
-        
         fig = Figure(figsize=(width, height), dpi=dpi)
         self.axes = fig.add_subplot(111)
 
         FigureCanvas.__init__(self, fig)
         self.setParent(parent)
-
-        FigureCanvas.setSizePolicy(self, QSizePolicy.Expanding, QSizePolicy.Expanding)
+        FigureCanvas.setSizePolicy(self, QtWidgets.QSizePolicy.Expanding,
+                                         QtWidgets.QSizePolicy.Expanding)
         FigureCanvas.updateGeometry(self)
 
         self.initPlot()
-
         self.mpl_connect('button_press_event', self.onClick)
-
         self.draw()
 
     def initPlot(self):
@@ -29,4 +28,8 @@ class Canvas(FigureCanvas):
 
     def onClick(self, event):
         """Docstring"""
-        pass
+        if event.inaxes is not None:
+            signal = {'name':'CanvasClicked',
+                      'xdata':event.xdata,
+                      'ydata':event.ydata}
+            self.canvasClicked.emit(signal)
--- ATEMview/DecayWindow.py
+++ ATEMview/DecayWindow.py
@@ -1,34 +1,14 @@
 
 import numpy as np
-from PyQt5.QtCore import pyqtSignal
-from PyQt5.QtWidgets import QVBoxLayout
+from PyQt5 import QtCore, QtWidgets
 from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
 from Canvas import Canvas
 from ATEMWidget import ATEMWidget
 
-class DecayCanvas(Canvas):
-    """ Docstring """
-
-    locClicked = pyqtSignal(dict, name='locClicked')
-
-    def __init__(self, parent=None, width=5, height=4, dpi=100):
-        """Docstring"""
-        super().__init__(parent, width, height, dpi)
-        self.axes.set_yscale('log')
-        self.axes.set_xscale('log')
-
-    def initPlot(self):
-        """Docstring"""
-        self.obsPlot, = self.axes.plot([], [], '-o', c='k', ms=2., label="Obs")
-        self.predPlot, = self.axes.plot([], [], '-o', c='r', ms=2., label="Pred")
-        self.uncertBounds = self.axes.fill_between([], [], alpha=0.3, color='k')
-        self.timePlot, = self.axes.plot([], [], ':', color='green', zorder = -1)
-        self.axes.legend()
-
 class DecayWidget(ATEMWidget):
     """docstring for LocWidget"""
     def __init__(self, parent):
-        super(DecayWidget, self).__init__(parent)
+        super().__init__(parent)
         self.parent = parent
         self.init_ui()
         self.init_signals()
@@ -36,15 +16,29 @@ class DecayWidget(ATEMWidget):
 
     def init_ui(self):
         """ Docstring """
-        self.dc = DecayCanvas(parent=self, width=5, height=4, dpi=100)
-        toolbar = NavigationToolbar(self.dc, self, coordinates=False)
+        self.canvas = Canvas(parent=self, width=5, height=4, dpi=100)
+        self.obsPlot, = self.canvas.axes.plot([], [], '-o', c='k', ms=2., label="Obs")
+        self.predPlot, = self.canvas.axes.plot([], [], '-o', c='r', ms=2., label="Pred")
+        self.uncertBounds = self.canvas.axes.fill_between([], [], alpha=0.3, color='k')
+        self.timePlot, = self.canvas.axes.plot([], [], ':', color='green', zorder = -1)
+        self.canvas.axes.legend()
+        self.canvas.axes.set_yscale('log')
+        self.canvas.axes.set_xscale('log')
 
-        l = QVBoxLayout(self)
-        l.addWidget(self.dc)
+        toolbar = NavigationToolbar(self.canvas, self, coordinates=False)
+
+        l = QtWidgets.QVBoxLayout(self)
+        l.addWidget(self.canvas)
         l.addWidget(toolbar)
 
     def init_signals(self):
-        self.dc.locClicked.connect(self.parent.get_event)
+        self.canvas.canvasClicked.connect(self.canvasClicked)
+
+    @QtCore.pyqtSlot(dict)
+    def canvasClicked(self, event):
+        signal = {'name':'closestTime',
+                  't':event['xdata']}
+        self.ChangeSelectionSignal.emit(signal)
 
     def setLocation(self, loc):
         """ Docstring """
@@ -55,20 +49,19 @@ class DecayWidget(ATEMWidget):
         lower = obs - loc.dBdt_Z_uncert.values
         upper = obs + loc.dBdt_Z_uncert.values
 
-        self.dc.obsPlot.set_data(t, obs)
-        self.dc.predPlot.set_data(t, pred)
+        self.obsPlot.set_data(t, obs)
+        self.predPlot.set_data(t, pred)
 
         tv = np.r_[t[0], t, t[-1], t[::-1], t[0]]
         v = np.r_[lower[0], upper, lower[-1], lower[::-1], lower[0]]
-        self.dc.uncertBounds.set_paths([np.c_[tv, v]])
+        self.uncertBounds.set_paths([np.c_[tv, v]])
 
-        self.dc.axes.set_xlim(t.min(), t.max())
-        self.dc.axes.set_ylim(loc.dBdt_Z.min(), loc.dBdt_Z.max())
-        self.dc.axes.set_title(loc.locInd.iloc[0])
-        self.dc.draw()
+        self.canvas.axes.set_xlim(t.min(), t.max())
+        self.canvas.axes.set_ylim(loc.dBdt_Z.min(), loc.dBdt_Z.max())
+        self.canvas.axes.set_title(loc.locInd.iloc[0])
+        self.canvas.draw()
 
     def setTime(self, time):
         """ docstring """
-        self.dc.timePlot.set_data([time, time], [1e-20, 1e20])
-        self.dc.draw()
-
+        self.timePlot.set_data([time, time], [1e-20, 1e20])
+        self.canvas.draw()
