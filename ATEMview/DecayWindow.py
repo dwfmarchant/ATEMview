12e0060f7ac5f8df833cd3d8b3870f7209244baa
Dave Marchant <dwfmarchant@gmail.com>
2017-06-10 12:00:01 -0700

    Converted Decay window to pyqtgraph.

--- ATEMview/DecayWindow.py
+++ ATEMview/DecayWindow.py
@@ -1,9 +1,12 @@
 
 import numpy as np
 from PyQt5 import QtCore, QtWidgets
-from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
-from .Canvas import Canvas
+import pyqtgraph as pg
 from .ATEMWidget import ATEMWidget
+
+pg.setConfigOption('background', 'w')
+pg.setConfigOption('foreground', 'k')
+pg.setConfigOptions(antialias=True)
 
 class DecayWidget(ATEMWidget):
     """docstring for LocWidget"""
@@ -15,50 +18,118 @@ class DecayWidget(ATEMWidget):
 
     def init_ui(self):
         """ Docstring """
-        self.canvas = Canvas(parent=self, width=5, height=4, dpi=100)
-        self.obsPlot, = self.canvas.axes.plot([], [], '-o', c='k', ms=2., label="Obs")
-        self.predPlot, = self.canvas.axes.plot([], [], '-o', c='r', ms=2., label="Pred")
-        self.uncertBounds = self.canvas.axes.fill_between([], [], alpha=0.3, color='k')
-        self.timePlot, = self.canvas.axes.plot([], [], ':', color='green', zorder = -1)
-        self.canvas.axes.legend()
-        self.canvas.axes.set_yscale('log')
-        self.canvas.axes.set_xscale('log')
-        self.canvas.canvasClicked.connect(self.canvasClicked)
 
-        toolbar = NavigationToolbar(self.canvas, self, coordinates=False)
+        # Make the background white
+        palette = self.palette()
+        palette.setColor(self.backgroundRole(), QtCore.Qt.white)
+        self.setPalette(palette)
+
+        self.titleLabel = QtWidgets.QLabel()
+        self.titleLabel.setAlignment(QtCore.Qt.AlignCenter)
+
+
+        self.win = pg.GraphicsWindow()
+        # self.win = pg.GraphicsWidget()
+        self.chLabel = pg.LabelItem(justify='right')
+        self.win.addItem(self.chLabel)
+
+        self.plotWidget = self.win.addPlot(row=1, col=0)
+        self.plotWidget.setLogMode(x=True, y=True)
+        self.plotWidget.setLabel('left', 'dB/dt')
+        self.plotWidget.setLabel('bottom', 'Time', units='s')
+        legend = self.plotWidget.addLegend(offset=(450, 30))
+        self.plotWidget.showGrid(x=True, y=True)
+
+        self.obsPlot = pg.PlotDataItem(symbol='o',
+                                       symbolSize=5,
+                                       symbolBrush='k',
+                                       pen={'color': 'k',
+                                            'width': 2},
+                                       name='Obs.')
+
+        self.predPlot = pg.PlotDataItem(symbol='o',
+                                        symbolSize=5,
+                                        symbolBrush='r',
+                                        pen={'color': 'r',
+                                             'width': 2},
+                                        name='Pred.')
+
+        self.selectedTimeLine = pg.InfiniteLine(angle=90,
+                                                movable=False,
+                                                pen={'color':'k',
+                                                     'width':2,
+                                                     'style':QtCore.Qt.DotLine})
+
+
+        self.lowerPlot = pg.PlotDataItem()
+        self.upperPlot = pg.PlotDataItem()
+        uncertBounds = pg.FillBetweenItem(self.lowerPlot, self.upperPlot, 0.8)
+
+        # Crosshair
+        self.chvLine = pg.InfiniteLine(angle=90, movable=False, pen={'color':'k', 'width':0.25})
+        self.chhLine = pg.InfiniteLine(angle=0, movable=False, pen={'color':'k', 'width':0.25})
+
+        self.plotWidget.addItem(self.chvLine, ignoreBounds=True)
+        self.plotWidget.addItem(self.chhLine, ignoreBounds=True)
+        self.plotWidget.addItem(self.obsPlot)
+        self.plotWidget.addItem(self.predPlot)
+        self.plotWidget.addItem(self.selectedTimeLine, ignoreBounds=True)
+        self.plotWidget.addItem(uncertBounds, ignoreBounds=True)
+        self.plotWidget.addItem(self.lowerPlot, ignoreBounds=True)
+        self.plotWidget.addItem(self.upperPlot, ignoreBounds=True)
+
+        uncertBounds.setZValue(0)
+        self.selectedTimeLine.setZValue(1)
+        self.obsPlot.setZValue(2)
+        self.predPlot.setZValue(3)
+        self.chvLine.setZValue(4)
+        self.chhLine.setZValue(5)
+        legend.setZValue(6)
 
         l = QtWidgets.QVBoxLayout(self)
-        l.addWidget(self.canvas)
-        l.addWidget(toolbar)
+        l.addWidget(self.titleLabel)
+        l.addWidget(self.win)
 
-    @QtCore.pyqtSlot(dict)
-    def canvasClicked(self, event):
+        self.mouseMoveProxy = pg.SignalProxy(self.plotWidget.scene().sigMouseMoved,
+                                             rateLimit=30,
+                                             slot=self.mouseMovedEvent)
+        self.plotWidget.scene().sigMouseClicked.connect(self.clickEvent)
+
+    def mouseMovedEvent(self, pos):
+        pos = pos[0]
+        if self.plotWidget.sceneBoundingRect().contains(pos):
+            mousePoint = self.plotWidget.getViewBox().mapSceneToView(pos)
+            string = "<span style='font-size: 12pt'>x={:.2e}, t={:.2e}</span>"
+            self.chLabel.setText(string.format(10**mousePoint.x(), 10**mousePoint.y()))
+            self.chvLine.setPos(mousePoint.x())
+            self.chhLine.setPos(mousePoint.y())
+
+    def clickEvent(self, event):
+        if self.plotWidget.sceneBoundingRect().contains(event.scenePos()):
+            mousePoint = self.plotWidget.getViewBox().mapSceneToView(event.scenePos())
         signal = {'name':'closestTime',
-                  't':event['xdata']}
+                  't':10**mousePoint.x()}
         self.ChangeSelectionSignal.emit(signal)
 
     def setLocation(self, loc):
         """ Docstring """
-
         t = loc.t.values
         obs = loc.dBdt_Z.values
         pred = loc.dBdt_Z_pred.values
         lower = obs - loc.dBdt_Z_uncert.values
         upper = obs + loc.dBdt_Z_uncert.values
+        lower[lower < 0.] = obs.min()/10.
 
-        self.obsPlot.set_data(t, obs)
-        self.predPlot.set_data(t, pred)
+        self.obsPlot.setData(t, obs)
+        self.predPlot.setData(t, pred)
+        self.upperPlot.setData(t, lower)
+        self.lowerPlot.setData(t, upper)
 
-        tv = np.r_[t[0], t, t[-1], t[::-1], t[0]]
-        v = np.r_[lower[0], upper, lower[-1], lower[::-1], lower[0]]
-        self.uncertBounds.set_paths([np.c_[tv, v]])
+        self.plotWidget.setXRange(np.log10(t.min()), np.log10(t.max()))
+        self.plotWidget.setYRange(np.log10(obs.min()), np.log10(obs.max()))
 
-        self.canvas.axes.set_xlim(t.min(), t.max())
-        self.canvas.axes.set_ylim(loc.dBdt_Z.min(), loc.dBdt_Z.max())
-        self.canvas.axes.set_title(loc.locInd.iloc[0])
-        self.canvas.draw()
+        self.titleLabel.setText('{}'.format(loc.locInd.iloc[0]))
 
     def setTime(self, time):
         """ docstring """
-        self.timePlot.set_data([time, time], [1e-20, 1e20])
-        self.canvas.draw()
+        self.selectedTimeLine.setPos(np.log10(time.values))
