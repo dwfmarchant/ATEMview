4937b027cb397be7665f61fabd5568119851de20
Dave Marchant <dwfmarchant@gmail.com>
2017-06-13 10:45:59 -0700

    Initial implimentation if LocWindow in pyqtgraph.

--- ATEMview/LocWindow.py
+++ ATEMview/LocWindow.py
@@ -1,8 +1,9 @@
 """ ATEMview Location Window """
 from PyQt5 import QtCore, QtWidgets
-from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
-from .Canvas import Canvas
+import pyqtgraph as pg
+import numpy as np
 from .ATEMWidget import ATEMWidget
+from .colormaps import jetCM, jetBrush
 
 class LocWidget(ATEMWidget):
     """docstring for LocWidget"""
@@ -11,13 +12,68 @@ class LocWidget(ATEMWidget):
         super(LocWidget, self).__init__(parent)
         self.parent = parent
         self.init_ui()
-        self.init_plot()
+        self.showData = False
+        self.data = None
+        self.x = None
+        self.y = None
+        self.minVal = 1.
+        self.maxVal = 1.
         self.show()
 
     def init_ui(self):
         """ Docstring """
-        self.canvas = Canvas(parent=self, width=5, height=4, dpi=100)
-        self.canvas.canvasClicked.connect(self.canvasClicked)
+
+        # Make the background white
+        palette = self.palette()
+        palette.setColor(self.backgroundRole(), QtCore.Qt.white)
+        self.setPalette(palette)
+
+        self.plotWidget = pg.PlotWidget(enableMenu=False)
+        self.plotWidget.setLabel('left', 'Easting', units='m')
+        self.plotWidget.setLabel('bottom', 'Northing', units='m')
+        self.plotWidget.showGrid(x=True, y=True)
+        self.plotWidget.getViewBox().setAspectLocked()
+
+        self.scatter = pg.ScatterPlotItem(pen=None, pxMode=True)
+        self.plotWidget.addItem(self.scatter)
+
+        self.selectedLocVline = pg.InfiniteLine(angle=90,
+                                                movable=False,
+                                                pen={'color':'k',
+                                                     'width':2,
+                                                     'style':QtCore.Qt.DotLine})
+        self.plotWidget.addItem(self.selectedLocVline, ignoreBounds=True)
+
+        self.selectedLocHline = pg.InfiniteLine(angle=0,
+                                                movable=False,
+                                                pen={'color':'k',
+                                                     'width':2,
+                                                     'style':QtCore.Qt.DotLine})
+        self.plotWidget.addItem(self.selectedLocHline, ignoreBounds=True)
+
+        self.plotWidget.scene().sigMouseClicked.connect(self.clickEvent)
+
+        self.colorbarWidget = pg.PlotWidget(enableMenu=False)
+        self.colorbarWidget.setMaximumWidth(100)
+        self.colorbarWidget.getViewBox().setMouseEnabled(False, False)
+        self.colorbarWidget.setXRange(0, 20, padding=0)
+        self.colorbarWidget.setYRange(0, 256, padding=0)
+        self.colorbarWidget.getAxis('bottom').setPen(None)
+        self.colorbarWidget.getAxis('left').setPen(None)
+        self.colorbarWidget.setVisible(False)
+
+        self.cbMinLabel = QtWidgets.QLabel()
+        self.cbMinLabel.setAlignment(QtCore.Qt.AlignRight | QtCore.Qt.AlignVCenter)
+        self.cbMinLabel.setText('0.00')
+        self.cbMaxLabel = QtWidgets.QLabel()
+        self.cbMaxLabel.setAlignment(QtCore.Qt.AlignRight | QtCore.Qt.AlignVCenter)
+        self.cbMaxLabel.setText('1.00')
+
+        self.colorbar = pg.ImageItem()
+        cbData = np.arange(0, 256)[:, np.newaxis].repeat(20, axis=1).T
+        self.colorbar.setImage(jetCM[cbData])
+
+        self.colorbarWidget.addItem(self.colorbar)
 
         self.misfitCheckBox = QtWidgets.QCheckBox('Show Misfit')
         self.misfitCheckBox.toggled.connect(self.toggleMisfit)
@@ -26,75 +82,83 @@ class LocWidget(ATEMWidget):
         self.maxCvalSlider.setMaximum(100)
         self.maxCvalSlider.setValue(100)
         self.maxCvalSlider.valueChanged.connect(self.setClim)
+        self.maxCvalSlider.setVisible(False)
 
-        toolbar = NavigationToolbar(self.canvas, self, coordinates=False)
+        cbvLayout = QtWidgets.QVBoxLayout()
+        cbvLayout.addWidget(self.cbMaxLabel)
+        cbvLayout.addWidget(self.colorbarWidget)
+        cbvLayout.addWidget(self.cbMinLabel)
+
+        hLayout = QtWidgets.QHBoxLayout()
+        hLayout.addWidget(self.plotWidget)
+        hLayout.addLayout(cbvLayout)
+
         vLayout = QtWidgets.QVBoxLayout(self)
         vLayout.addWidget(self.misfitCheckBox)
-        vLayout.addWidget(self.canvas)
-        vLayout.addWidget(toolbar)
+        vLayout.addLayout(hLayout)
+
         vLayout.addWidget(self.maxCvalSlider)
 
-    def init_plot(self):
-        """ Initialize the plot """
-        self.selCrossX, = self.canvas.axes.plot([], [], '--', c='r', lw=0.5, zorder=0)
-        self.selCrossY, = self.canvas.axes.plot([], [], '--', c='r', lw=0.5, zorder=1)
-        self.allPlot, = self.canvas.axes.plot([], [], 'o', c='k', ms=1., zorder=2)
-        self.selPlot, = self.canvas.axes.plot([], [], 'o', c='r', ms=2., zorder=3)
-        self.scatter = self.canvas.axes.scatter([], [], c=[], s=5, cmap='jet',
-                                                zorder=4, visible=False)
-        self.cbar = self.canvas.fig.colorbar(self.scatter, pad=0., aspect=40)
-        self.cbar.ax.set_visible(False)
-        self.canvas.axes.set_aspect('equal')
 
-    @QtCore.pyqtSlot(dict)
-    def canvasClicked(self, event):
-        """ Callback that gets fired when the plot axis is clicked """
-        signal = {'name':'closestLoc',
-                  'x':event['xdata'],
-                  'y':event['ydata']}
-        self.ChangeSelectionSignal.emit(signal)
+    def clickEvent(self, event):
+        if self.plotWidget.sceneBoundingRect().contains(event.scenePos()):
+            mousePoint = self.plotWidget.getViewBox().mapSceneToView(event.scenePos())
+            signal = {'name':'closestLoc',
+                      'x':mousePoint.x(),
+                      'y':mousePoint.y()}
+            self.ChangeSelectionSignal.emit(signal)
+        else:
+            pass
 
     @QtCore.pyqtSlot(bool)
     def toggleMisfit(self, show):
         """ Callback that gets fired 'Show Misfit' box is toggled """
         if show:
-            if not self.scatter.get_array().any():
-                signal = {'name':'sameTime'}
-                self.ChangeSelectionSignal.emit(signal)
-            self.scatter.set_visible(True)
-            self.cbar.ax.set_visible(True)
+            self.colorbarWidget.setVisible(True)
+            self.maxCvalSlider.setVisible(True)
+            self.showData = True
         else:
-            self.scatter.set_visible(False)
-            self.cbar.ax.set_visible(False)
-        self.canvas.draw()
+            self.colorbarWidget.setVisible(False)
+            self.maxCvalSlider.setVisible(False)
+            self.showData = False
+        self.updatePlot()
+
+    def updatePlot(self):
+        if self.showData:
+            hsVal = self.maxCvalSlider.value()
+            maxVal = self.maxVal*hsVal/100.
+            self.cbMaxLabel.setText('{:.2f}'.format(maxVal))
+            bins = np.linspace(0., maxVal, 255)
+            di = np.digitize(self.data, bins)-1
+            self.scatter.setData(self.x, self.y,
+                                 pen=None,
+                                 brush=jetBrush[di],
+                                 symbolSize=10.)
+        else:
+            self.scatter.setData(self.x, self.y, pen=None, brush='k', symbolSize=10.)
 
     def setAll(self, x, y):
         """ Docstring """
-        self.allPlot.set_data(x, y)
-        self.canvas.axes.set_xlim(x.min()-100., x.max()+100.)
-        self.canvas.axes.set_ylim(y.min()-100., y.max()+100.)
-        self.canvas.draw()
+        self.scatter.setData(x, y, pen=None, brush='k', symbolSize=10.)
+        self.plotWidget.setXRange(x.min()-100., x.max()+100.)
+        self.plotWidget.setYRange(y.min()-100., y.max()+100.)
 
     def setLocation(self, loc):
         """ Docstring """
-        x = loc.iloc[0].x
-        y = loc.iloc[0].y
-        self.selPlot.set_data(x, y)
-        self.selCrossX.set_data(self.canvas.axes.get_xlim(), [y, y])
-        self.selCrossY.set_data([x, x], self.canvas.axes.get_ylim())
-        self.canvas.draw()
+        xl = loc.iloc[0].x
+        yl = loc.iloc[0].y
+        self.selectedLocVline.setPos(xl)
+        self.selectedLocHline.setPos(yl)
 
     def setTime(self, data_times):
         """ Set the displayed misfit data """
-        misfit = (data_times.dBdt_Z-data_times.dBdt_Z_pred).abs()/data_times.dBdt_Z_uncert
-        self.scatter.set_offsets(data_times[['x', 'y']])
-        self.scatter.set_array(misfit)
-        self.setClim()
+        self.x = data_times.x.values
+        self.y = data_times.y.values
+        self.data = (data_times.dBdt_Z-data_times.dBdt_Z_pred).abs()/data_times.dBdt_Z_uncert
+        self.minVal = self.data.min()
+        self.maxVal = self.data.max()
+        self.updatePlot()
 
     def setClim(self):
         """ Set the color limits on the misfit scatter plot """
-        hsVal = self.maxCvalSlider.value()
-        misfitRange = self.scatter.get_array().max() - self.scatter.get_array().min()
-        clMax = self.scatter.get_array().min()+misfitRange*hsVal/100.
-        self.scatter.set_clim(0., clMax)
-        self.canvas.draw()
+        self.updatePlot()
