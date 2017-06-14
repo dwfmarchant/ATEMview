f086cba9e29132682019d95c27486a0be438567e
Dave Marchant <dwfmarchant@gmail.com>
2017-06-13 23:18:18 -0700

    Converted GridWindow to pyqtgraph.  Removed Canvas class.

--- ATEMview/GridWindow.py
+++ ATEMview/GridWindow.py
@@ -1,10 +1,10 @@
 
 from PyQt5 import QtCore, QtWidgets
-from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
 import numpy as np
+import pyqtgraph as pg
 from .ATEMWidget import ATEMWidget
-from .Canvas import Canvas
 from .GridWorker import GridWorker
+from .colormaps import jetCM
 
 class GridWidget(ATEMWidget):
     """docstring for GridWidget"""
@@ -16,6 +16,7 @@ class GridWidget(ATEMWidget):
         self.init_grids()
         self.init_ui()
 
+        self.current_tInd = self.parent.selectedTimeInd
         self.absMinValue = 1e-20
         self.absMaxValue = 1e20
 
@@ -24,25 +25,67 @@ class GridWidget(ATEMWidget):
     def init_ui(self):
         """ Docstring """
 
-        imshowOpts = {'origin':'lower',
-                      'cmap':'jet',
-                      'interpolation':'bilinear'}
+        # Make the background white
+        palette = self.palette()
+        palette.setColor(self.backgroundRole(), QtCore.Qt.white)
+        self.setPalette(palette)
 
-        self.obsCanvas = Canvas(parent=self)
-        obsToolbar = NavigationToolbar(self.obsCanvas, self, coordinates=False)
-        self.obs_im = self.obsCanvas.axes.imshow([[0.]], **imshowOpts)
-        self.selCrossXobs, = self.obsCanvas.axes.plot([], [], '--', c='r', lw=0.5, zorder=0)
-        self.selCrossYobs, = self.obsCanvas.axes.plot([], [], '--', c='r', lw=0.5, zorder=0)
-        self.obsCanvas.fig.colorbar(self.obs_im)
-        self.obsCanvas.canvasClicked.connect(self.canvasClicked)
+        self.obsPlotWidget = pg.PlotWidget(enableMenu=False)
+        self.obsPlotWidget.setLabel('left', 'Easting', units='m')
+        self.obsPlotWidget.setLabel('bottom', 'Northing', units='m')
+        self.obsPlotWidget.showGrid(x=True, y=True)
+        self.obsPlotWidget.getViewBox().setAspectLocked()
+        self.obsPlotWidget.setTitle('Observed')
 
-        self.predCanvas = Canvas(parent=self, share_ax=self.obsCanvas.axes)
-        predToolbar = NavigationToolbar(self.predCanvas, self, coordinates=False)
-        self.pred_im = self.predCanvas.axes.imshow([[0.]], **imshowOpts)
-        self.selCrossXpred, = self.predCanvas.axes.plot([], [], '--', c='r', lw=0.5, zorder=0)
-        self.selCrossYpred, = self.predCanvas.axes.plot([], [], '--', c='r', lw=0.5, zorder=0)
-        self.predCanvas.fig.colorbar(self.pred_im)
-        self.predCanvas.canvasClicked.connect(self.canvasClicked)
+        self.predPlotWidget = pg.PlotWidget(enableMenu=False)
+        self.predPlotWidget.setLabel('left', 'Easting', units='m')
+        self.predPlotWidget.setLabel('bottom', 'Northing', units='m')
+        self.predPlotWidget.showGrid(x=True, y=True)
+        self.predPlotWidget.getViewBox().setAspectLocked()
+        self.predPlotWidget.setTitle('Predicted')
+
+        self.colorbarWidget = pg.PlotWidget(enableMenu=False)
+        self.colorbarWidget.setMaximumWidth(40)
+        self.colorbarWidget.getViewBox().setMouseEnabled(False, False)
+        self.colorbarWidget.setXRange(0, 20, padding=0)
+        self.colorbarWidget.setYRange(0, 256, padding=0)
+        self.colorbarWidget.getAxis('bottom').setPen(None)
+        self.colorbarWidget.getAxis('left').setPen(None)
+        self.colorbarWidget.getAxis('left').setWidth(0)
+        self.colorbarWidget.getAxis('right').setWidth(0)
+        self.colorbarWidget.getAxis('top').setWidth(0)
+        self.colorbarWidget.getAxis('bottom').setWidth(0)
+
+        self.cbMinLabel = QtWidgets.QLabel()
+        self.cbMinLabel.setText('')
+        self.cbMaxLabel = QtWidgets.QLabel()
+        self.cbMaxLabel.setText('')
+
+        self.colorbar = pg.ImageItem()
+        cbData = np.arange(0, 256)[:, np.newaxis].repeat(20, axis=1).T
+        self.colorbar.setImage(jetCM[cbData])
+        self.colorbarWidget.addItem(self.colorbar)
+
+        self.obsImage = pg.ImageItem()
+        self.obsPlotWidget.addItem(self.obsImage)
+
+        self.predImage = pg.ImageItem()
+        self.predPlotWidget.addItem(self.predImage)
+
+        locLinePen = {'color':'k', 'width':2, 'style':QtCore.Qt.DotLine}
+        self.selectedLocVlineObs = pg.InfiniteLine(angle=90, movable=False, pen=locLinePen)
+        self.selectedLocVlinePred = pg.InfiniteLine(angle=90, movable=False, pen=locLinePen)
+        self.obsPlotWidget.addItem(self.selectedLocVlineObs, ignoreBounds=True)
+        self.predPlotWidget.addItem(self.selectedLocVlinePred, ignoreBounds=True)
+        self.selectedLocHlineObs = pg.InfiniteLine(angle=0, movable=False, pen=locLinePen)
+        self.selectedLocHlinePred = pg.InfiniteLine(angle=0, movable=False, pen=locLinePen)
+        self.obsPlotWidget.addItem(self.selectedLocHlineObs, ignoreBounds=True)
+        self.predPlotWidget.addItem(self.selectedLocHlinePred, ignoreBounds=True)
+
+        self.obsPlotWidget.scene().sigMouseClicked.connect(self.clickObsEvent)
+        self.predPlotWidget.scene().sigMouseClicked.connect(self.clickPredEvent)
+        self.obsPlotWidget.setXLink(self.predPlotWidget)
+        self.obsPlotWidget.setYLink(self.predPlotWidget)
 
         self.highSlider = QtWidgets.QSlider(QtCore.Qt.Horizontal)
         self.highSlider.setMaximum(100)
@@ -54,17 +97,15 @@ class GridWidget(ATEMWidget):
         self.lowSlider.setValue(0)
         self.lowSlider.valueChanged.connect(self.setClim)
 
-        lc1 = QtWidgets.QVBoxLayout()
-        lc1.addWidget(self.obsCanvas)
-        lc1.addWidget(obsToolbar)
-
-        lc2 = QtWidgets.QVBoxLayout()
-        lc2.addWidget(self.predCanvas)
-        lc2.addWidget(predToolbar)
-
         lh = QtWidgets.QHBoxLayout()
-        lh.addLayout(lc1)
-        lh.addLayout(lc2)
+        lh.addWidget(self.obsPlotWidget)
+        lh.addWidget(self.predPlotWidget)
+
+        cbvLayout = QtWidgets.QVBoxLayout()
+        cbvLayout.addWidget(self.cbMaxLabel)
+        cbvLayout.addWidget(self.colorbarWidget)
+        cbvLayout.addWidget(self.cbMinLabel)
+        lh.addLayout(cbvLayout)
 
         l = QtWidgets.QVBoxLayout(self)
         l.addLayout(lh)
@@ -89,17 +130,43 @@ class GridWidget(ATEMWidget):
         if not event['ch'] in self.gridStore:
             self.gridStore[event['ch']] = {}
         self.gridStore[event['ch']][event['tInd']] = event
+        if event['tInd'] == self.current_tInd:
+            if event['ch'] == 'dBdt_Z':
+                self.absMinValue = np.nanmin(event['grid'])
+                self.absMaxValue = np.nanmax(event['grid'])
+                self.drawObs()
+                try:
+                    self.drawPred()
+                except Exception as e:
+                    pass
+                self.obsPlotWidget.autoRange()
+            if event['ch'] == 'dBdt_Z_pred':
+                self.drawPred()
+                try:
+                    self.drawObs()
+                except Exception as e:
+                    pass
+                self.predPlotWidget.autoRange()
 
-    def draw(self):
-        self.obsCanvas.draw()
-        self.predCanvas.draw()
+    def clickObsEvent(self, event):
+        if self.obsPlotWidget.sceneBoundingRect().contains(event.scenePos()):
+            mousePoint = self.obsPlotWidget.getViewBox().mapSceneToView(event.scenePos())
+            signal = {'name':'closestLoc',
+                      'x':mousePoint.x(),
+                      'y':mousePoint.y()}
+            self.ChangeSelectionSignal.emit(signal)
+        else:
+            pass
 
-    @QtCore.pyqtSlot(dict)
-    def canvasClicked(self, event):
-        signal = {'name':'closestLoc',
-                  'x':event['xdata'],
-                  'y':event['ydata']}
-        self.ChangeSelectionSignal.emit(signal)
+    def clickPredEvent(self, event):
+        if self.predPlotWidget.sceneBoundingRect().contains(event.scenePos()):
+            mousePoint = self.predPlotWidget.getViewBox().mapSceneToView(event.scenePos())
+            signal = {'name':'closestLoc',
+                      'x':mousePoint.x(),
+                      'y':mousePoint.y()}
+            self.ChangeSelectionSignal.emit(signal)
+        else:
+            pass
 
     def setClim(self):
         lsVal = self.lowSlider.value()
@@ -107,49 +174,64 @@ class GridWidget(ATEMWidget):
         if lsVal >= hsVal:
             self.lowSlider.setValue(hsVal-1)
             lsVal = self.lowSlider.value()
+        self.drawObs()
+        self.drawPred()
+
+    def getClim(self):
+        lsVal = self.lowSlider.value()
+        hsVal = self.highSlider.value()
         dv = self.absMaxValue-self.absMinValue
         clMin = self.absMinValue+dv*lsVal/100.
         clMax = self.absMinValue+dv*hsVal/100.
-        self.obs_im.set_clim(clMin, clMax)
-        self.pred_im.set_clim(clMin, clMax)
-        self.draw()
+        return clMin, clMax
 
     def setLocation(self, loc):
         """ Docstring """
-        x = loc.iloc[0].x
-        y = loc.iloc[0].y
-        self.selCrossXobs.set_data(self.obsCanvas.axes.get_xlim(), [y, y])
-        self.selCrossYobs.set_data([x, x], self.obsCanvas.axes.get_ylim())
-        self.selCrossXpred.set_data(self.predCanvas.axes.get_xlim(), [y, y])
-        self.selCrossYpred.set_data([x, x], self.predCanvas.axes.get_ylim())
-        self.draw()
+        xl = loc.iloc[0].x
+        yl = loc.iloc[0].y
+        self.selectedLocVlineObs.setPos(xl)
+        self.selectedLocHlineObs.setPos(yl)
+        self.selectedLocVlinePred.setPos(xl)
+        self.selectedLocHlinePred.setPos(yl)
 
     def setTime(self, data_times):
 
-        tInd = data_times.iloc[0].tInd
-
+        self.current_tInd = data_times.iloc[0].tInd
         if 'dBdt_Z' in self.gridStore:
-            grid_obs = self.gridStore['dBdt_Z'][tInd]['grid']
-            x_vector = self.gridStore['dBdt_Z'][tInd]['x_vector']
-            y_vector = self.gridStore['dBdt_Z'][tInd]['y_vector']
-            self.obs_im.set_data(grid_obs)
-            self.obs_im.set_extent((x_vector[0], x_vector[-1],
-                                    y_vector[0], y_vector[-1]))
-
+            grid_obs = self.gridStore['dBdt_Z'][self.current_tInd]['grid']
             self.absMinValue = np.nanmin(grid_obs)
             self.absMaxValue = np.nanmax(grid_obs)
-
+            self.drawObs()
         if 'dBdt_Z_pred' in self.gridStore:
-            grid_pred = self.gridStore['dBdt_Z_pred'][tInd]['grid']
-            x_vector = self.gridStore['dBdt_Z_pred'][tInd]['x_vector']
-            y_vector = self.gridStore['dBdt_Z_pred'][tInd]['y_vector']
+            self.drawPred()
 
-            if np.any(grid_pred):
-                self.pred_im.set_data(grid_pred)
-                self.pred_im.set_extent((x_vector[0], x_vector[-1],
-                                         y_vector[0], y_vector[-1]))
-            else:
-                self.pred_im.set_data(np.nan*np.ones((2, 2)))
+    def drawObs(self):
+        grid_obs = self.gridStore['dBdt_Z'][self.current_tInd]['grid'].T
+        x_vector = self.gridStore['dBdt_Z'][self.current_tInd]['x_vector']
+        y_vector = self.gridStore['dBdt_Z'][self.current_tInd]['y_vector']
+        clMin, clMax = self.getClim()
+        bins = np.linspace(clMin, clMax, 255)
+        cInd_obs = np.digitize(grid_obs, bins)
+        colors_obs = jetCM[cInd_obs]
+        colors_obs[np.isnan(grid_obs)] = (0, 0, 0, 0)
+        self.obsImage.setImage(colors_obs)
+        self.obsImage.setPos(x_vector.min(), y_vector.min())
+        self.obsImage.setScale(x_vector[1]-x_vector[0])
+        self.cbMaxLabel.setText('{:.2e}'.format(clMax))
+        self.cbMinLabel.setText('{:.2e}'.format(clMin))
 
-        self.setClim()
-        self.draw()
+    def drawPred(self):
+        grid_pred = self.gridStore['dBdt_Z_pred'][self.current_tInd]['grid'].T
+        x_vector = self.gridStore['dBdt_Z_pred'][self.current_tInd]['x_vector']
+        y_vector = self.gridStore['dBdt_Z_pred'][self.current_tInd]['y_vector']
+        if np.any(grid_pred):
+            clMin, clMax = self.getClim()
+            bins = np.linspace(clMin, clMax, 255)
+            cInd_pred = np.digitize(grid_pred, bins)
+            colors_pred = jetCM[cInd_pred]
+            colors_pred[np.isnan(grid_pred)] = (0, 0, 0, 0)
+            self.predImage.setImage(colors_pred)
+            self.predImage.setPos(x_vector.min(), y_vector.min())
+            self.predImage.setScale(x_vector[1]-x_vector[0])
+        else:
+            self.predImage.setImage(None)
